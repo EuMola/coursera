01378a7d17bbb9a2da9b26888f146f7a4831ae84
EuMola <eu@eumola.com>
2016-11-01 19:00:59 +0100

    sign-up.html

---
+++ assigment5/src/public/customer/sign-up.controller.js
@@ -0,0 +1,14 @@
+(function () {
+
+angular.module('public')
+.controller('SignUpController', SignUpController);
+
+function SignUpController() {
+  var sign = this;
+
+  sign.submit = function () {
+    sign.completed = true;
+  };
+}
+
+})();
--- assigment5/src/public/public.routes.js
+++ assigment5/src/public/public.routes.js
@@ -40,6 +40,10 @@ function routeConfig ($stateProvider) {
           return MenuService.getMenuItems($stateParams.category);
         }]
       }
+    })
+    .state('public.signup', {
+      url: '/signup',
+      templateUrl: 'src/public/customer/sign-up.html'
     });
 }
 })();
